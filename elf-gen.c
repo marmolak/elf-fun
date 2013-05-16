64e342604812279deafe97f0fb7dbc23e5f589b4
Robin Hack <rhack@redhat.com>
2013-05-16 08:22:24 +0200

    Added stack to elf binary.

--- elf-gen.c
+++ elf-gen.c
@@ -1,14 +1,22 @@
 #include <elf.h>
 #include <stdio.h>
 #include <string.h>
+#include <assert.h>
+#include <unistd.h>
 
 int main (char argc, char **argv)
 {
     Elf64_Ehdr e;
+    /* code */
     Elf64_Phdr p;
+    /* stack */
+    Elf64_Phdr s;
+
+    const long page_size = sysconf (_SC_PAGESIZE);
 
     memset (&e, 0, sizeof (e));
     memset (&p, 0, sizeof (p));
+    memset (&s, 0, sizeof (s));
 
     e.e_ident [EI_MAG0] = ELFMAG0;
     e.e_ident [EI_MAG1] = ELFMAG1;
@@ -25,26 +33,35 @@ int main (char argc, char **argv)
     e.e_version = EV_CURRENT;
     e.e_ehsize = sizeof (e);
     e.e_phentsize = sizeof (p);
-    e.e_phnum = 1;
+    e.e_phnum = 2;
     e.e_entry = 0x400000;
     e.e_phoff = sizeof (e);
 
+    /* code */
     p.p_type = PT_LOAD;
-    p.p_offset = 4096;
+    p.p_offset = page_size;
     p.p_vaddr = 0x400000;
-    p.p_filesz = 4096;
-    p.p_memsz = 4096;
+    p.p_filesz = page_size;
+    p.p_memsz = page_size;
     p.p_flags = ( PF_X | PF_R );
 
+    /* stack */
+    s.p_type = PT_GNU_STACK;
+    s.p_flags = ( PF_W | PF_R );
 
-    char code[4096];
+
+    assert (page_size >= (sizeof (e) + sizeof (p) + sizeof (s)));
+
+    char code[page_size];
 
     FILE *f = fopen ("elf.out", "w");
     fwrite (&e, sizeof (e), 1, f);
     fwrite (&p, sizeof (p), 1, f);
-    fseek (f, 4096, SEEK_SET);
-    memset (code, 0x90, 4096);
-    memcpy (code, "\xb8\x3c\x00\x00\x00\x48\x31\xff\x66\xff\xc7\x66\xff\xc7\x0f\x05", 16);
-    fwrite (code, 4096, 1, f);
+    fwrite (&s, sizeof (s), 1, f);
+
+    fseek (f, page_size, SEEK_SET);
+    memset (code, 0x90, page_size);
+    memcpy (code, "\x48\xb8\x9a\x02\x00\x00\x00\x00\x00\x00\x50\xb8\x3c\x00\x00\x00\x48\x31\xff\x66\xff\xc7\x66\xff\xc7\x0f\x05", 27);
+    fwrite (code, page_size, 1, f);
     fclose (f);
 }
